62a59a7c0f8d88b0a4702cadfb5894b5e19ef2e9
Jagannath-Developer <raojagannath11@gmail.com>
2023-04-18 14:09:10 +0530

    weboscket update message

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // const express=require('express');
 // const app=express();
 // const dotenv=require('dotenv').config();
-// const PORT=process.env.PORT ||8080;
+const PORT=process.env.PORT ||8080;
 
 // app.get("/",(req,res)=>{
 //     res.send("Welcome sample API app")
@@ -35,28 +35,33 @@ const app = express();
 const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ noServer: true });
+
+
 // const server=new WebSoket.Server({port:'3002'});
 
-const server = app.listen(3002, () => {
-    console.log('Express server listening on port 3002');
+const server = app.listen(PORT, () => {
+  console.log(`Server run at PORT ${PORT} `);
+})
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit('connection', ws, request);
   });
-  
-  server.on('upgrade', (request, socket, head) => {
-    wss.handleUpgrade(request, socket, head, (ws) => {
-      wss.emit('connection', ws, request);
-    });
+});
+
+wss.on('connection', function connection(ws) {
+  console.log('Client connected');
+
+  ws.on('message', function incoming(message) {
+    console.log('recived:', message.toString());
+    wss.clients.forEach( function (client){
+          if(client!=ws){
+              client.send(""+message);
+          }
+      })
   });
 
-  wss.on('connection', function connection(ws) {
-    console.log('Client connected');
-  
-    ws.on('message', function incoming(message) {
-      console.log('Received message:', message.toString());
-    });
-  
-    ws.send('Hello, client!');
-  });
-
+  // ws.send('Hello, client!');
+});
 // server.on('connection',socket=>{
 //     socket.on('message',message=>{
 //         console.log("recived:",message.toString())
